01b77e45f7f44cfdce47e23f915e5949e1dfb203
nczimmerman00 <n.zman@eaglecom.net>
2022-11-01 18:43:46 -0500

    Added bootstrap and register functionality

--- ai_news/urls.py
+++ ai_news/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name= "logout")
 ]
